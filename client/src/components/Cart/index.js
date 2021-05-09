a4268f309dc4492573150c3ba61b43cfe9006770
Curtis Smith <cj1988333@hotmail.com>
2021-05-09 16:01:57 -0400

    finished adding payment functionality

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,10 +4,19 @@ import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
 import './style.css';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
 
     function toggleCart() {
       dispatch({ type: TOGGLE_CART });
@@ -24,12 +33,35 @@ const Cart = () => {
         }
     }, [state.cart.length, dispatch]);
 
+    useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+    }, [data]);
+
     function calculateTotal() {
         let sum = 0;
         state.cart.forEach(item => {
           sum += item.price * item.purchaseQuantity;
         });
         return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+      
+        state.cart.forEach((item) => {
+          for (let i = 0; i < item.purchaseQuantity; i++) {
+            productIds.push(item._id);
+          }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        });
+        //console.log(productIds);
     }
 
     if (!state.cartOpen) {
@@ -56,8 +88,8 @@ const Cart = () => {
         <strong>Total: ${calculateTotal()}</strong>
         {
           Auth.loggedIn() ?
-            <button>
-              Checkout
+            <button onClick={submitCheckout}>
+                Checkout
             </button>
             :
             <span>(log in to check out)</span>
